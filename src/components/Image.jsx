bcc4b6bdd40b94f3e38697141fb645979c5dcbd9
Yuudai Tamura <y.tamu63@gmail.com>
2024-10-13 11:22:56 +0900

    画像のパス変更

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,10 +2,10 @@ export default function Image() {
     return (
       <aside>
         <h2>Favourite photos</h2>
-        <a href="./assets/favorite-1.jpg"><img src="./assets/favorite-1_th.jpg" alt="Small black bird" /></a>
-        <a href="./assets/favorite-2.jpg"><img src="./assets/favorite-2_th.jpg" alt="Pretty bird with bright blue plumage" /></a>
-        <a href="./assets/favorite-3.jpg"><img src="./assets/favorite-3_th.jpg" alt="Large bird with white plumage" /></a>
-        <a href="./assets/favorite-4.jpg"><img src="./assets/favorite-4_th.jpg" alt="Large bird with black and white plumage" /></a>
+        <a href="../assets/favorite-1.jpg"><img src="../assets/favorite-1_th.jpg" alt="Small black bird" /></a>
+        <a href="../assets/favorite-2.jpg"><img src="../assets/favorite-2_th.jpg" alt="Pretty bird with bright blue plumage" /></a>
+        <a href="../assets/favorite-3.jpg"><img src="../assets/favorite-3_th.jpg" alt="Large bird with white plumage" /></a>
+        <a href="../assets/favorite-4.jpg"><img src="../assets/favorite-4_th.jpg" alt="Large bird with black and white plumage" /></a>
       </aside>
     );
   }
